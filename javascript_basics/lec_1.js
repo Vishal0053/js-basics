43f26417f196595d59f175e63012a555de1a9fb8
Vishal0053 <162530193+Vishal0053@users.noreply.github.com>
2025-09-08 04:40:39 +0000

    lec_1

--- javascript_basics/lec_1.js
+++ javascript_basics/lec_1.js
@@ -2,3 +2,6 @@
 const accountID = 143223
 let email = "abc@gmail.com"
 var password = "12345"
+
+// generally var is not used to declare variables usually its avoided because of non scopic behaviour
+
