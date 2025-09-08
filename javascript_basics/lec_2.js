3c33fffbaa88e7e9786ffbba3dd41f04cc62ef53
Vishal0053 <162530193+Vishal0053@users.noreply.github.com>
2025-09-08 05:08:35 +0000

    lec-2

--- javascript_basics/lec_2.js
+++ javascript_basics/lec_2.js
@@ -1,2 +1,29 @@
 // data types
-"use strict";
+
+/*
+String
+BigInt
+Number
+null
+undefined
+Symbol
+*/
+// String
+let nameOf = "Vishal"
+console.log(nameOf);
+
+// BigInt
+let Population = 2341567891223n
+console.log(Population);
+// Number
+let integer = 12
+let floating = 12.33
+console.log(integer);
+console.log(floating);
+// null - "StandAlone data type"
+let empty = null
+// undefined - "when the value is not assigned usually the undefined data type"
+let undefi
+console.log(typeof undefi);
+
+ 
