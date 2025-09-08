f19870ee6857c4194707486e23afd8263c5eb465
Vishal0053 <162530193+Vishal0053@users.noreply.github.com>
2025-09-08 08:57:05 +0000

    typecastingJavascript

---
+++ javascript_basics/conversion_operators.js
@@ -0,0 +1,10 @@
+let value = "-22"
+console.log(typeof(value));
+let Num = Number(value)
+console.log(typeof(Num));
+console.log(Num);
+let Yn = Boolean(Num)
+console.log(Yn);
+let BI = BigInt(value)
+console.log(BI);
+
