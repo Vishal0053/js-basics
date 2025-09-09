3be57802bdf094477c390d48db2c2239e47efc44
Vishal0053 <162530193+Vishal0053@users.noreply.github.com>
2025-09-09 08:02:53 +0000

    changes

---
+++ javascript_basics/05_strings.js
@@ -0,0 +1,22 @@
+// Strings
+
+// string concatenation
+let Name ="vishal"
+let age = 22
+let concatStr = "Hello my name is " + Name + " and My age is " +age;
+// console.log(concatStr);
+
+/* above is the way of cancatenating a string but can be avoided but instead we can use String 
+interpolation to perform the same task 
+*/
+// String Interpolation
+let ipStr = `Hello my name is ${Name} and my age is ${age}`
+// console.log(ipStr);
+const Gamename = new String("karan")
+
+// String Functions
+
+let Str1 = "kar"
+let str2 = "an"
+console.log(`concated String is ${Str1.concat(str2)}`);
+console.log(``)
