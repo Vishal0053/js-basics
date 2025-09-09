1652b60dcee2440786d4a67b7ec2b6e54aea347a
Vishal0053 <162530193+Vishal0053@users.noreply.github.com>
2025-09-09 06:00:37 +0000

    changes in file

--- javascript_basics/02_dataType.js
+++ javascript_basics/02_dataType.js
@@ -1,13 +1,20 @@
-// data types
-
 /*
+# Primitive Data types: call by value
 String
 BigInt
 Number
 null
+Boolean
 undefined
 Symbol
+
+# Reference (Non-Primitive)
+arrays
+objects
+functions
+
 */
+
 // String
 let nameOf = "Vishal"
 console.log(nameOf);
@@ -28,3 +35,23 @@ console.log(typeof undefi);
 // Symbol
 let unique = Symbol('dishant')
 console.log(typeof unique);
+
+// Non primitive data type : arrays,objects,functions
+
+// arrays
+const arr = [12,34,56];
+
+// objects
+let myObj = {
+    Name : "vishal",
+    age : 22
+};
+
+// functions
+
+let myfunc = function () {
+    console.log("this is a function");
+    
+}
+
+// info : in primitive data is stored in stack memory and non primitive data type is stored in heap memory
