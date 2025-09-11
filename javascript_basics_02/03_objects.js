d67332a5e1fecb9403a369650fbe51be48d9330a
Vishal0053 <162530193+Vishal0053@users.noreply.github.com>
2025-09-11 10:14:05 +0000

    objects editied

--- javascript_basics_02/03_objects.js
+++ javascript_basics_02/03_objects.js
@@ -1,10 +1,12 @@
 // objects in javascript
-
+let lastName = "Giri"
 const first_Obj = {
     id : Math.floor(Math.random()*1000+100),
     name : "Vishal Giri",
+    [lastName] : "Goshwami",
     balance : 12345
 } 
 // console.log(first_Obj.id);
 
 console.log(first_Obj["id"]);
+console.log(first_Obj[lastName]);
