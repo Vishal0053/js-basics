554c138fc6fa06e0b1696777b5fb4f8080c874ca
Vishal0053 <162530193+Vishal0053@users.noreply.github.com>
2025-09-11 04:42:15 +0000

    objects

---
+++ javascript_basics_02/03_objects.js
@@ -0,0 +1,10 @@
+// objects in javascript
+
+const first_Obj = {
+    id : Math.floor(Math.random()*1000+100),
+    name : "Vishal Giri",
+    balance : 12345
+} 
+// console.log(first_Obj.id);
+
+console.log(first_Obj["id"]);
