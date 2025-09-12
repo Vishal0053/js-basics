c4d20df104a2fe5783b7d602458dcc4261a4a17c
Vishal0053 <162530193+Vishal0053@users.noreply.github.com>
2025-09-12 11:07:30 +0000

    object basics

--- javascript_basics_02/03_objects.js
+++ javascript_basics_02/03_objects.js
@@ -1,12 +1,31 @@
-// objects in javascript
+// singleton
+// Object.create
+
+// objects literals in javascript
 let lastName = "Giri"
+
+const Mysym = Symbol("key1")
 const first_Obj = {
     id : Math.floor(Math.random()*1000+100),
     name : "Vishal Giri",
-    [lastName] : "Goshwami",
+    [Mysym] : "Goshwami",
     balance : 12345
 } 
 // console.log(first_Obj.id);
-
+first_Obj.balance =98765;
+// Object.freeze(first_Obj);
+first_Obj.balance =987609099;
 console.log(first_Obj["id"]);
-console.log(first_Obj[lastName]);
+console.log(first_Obj);
+
+first_Obj.greeting = function () {
+    console.log("hello Mr. How are you");
+}
+console.log(first_Obj.greeting());
+
+first_Obj.greetingtwo = function () {
+    console.log(`hello Mr.${this.name} How are you`);
+}
+
+console.log(first_Obj.greetingtwo());
+
